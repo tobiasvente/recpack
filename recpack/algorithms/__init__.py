96800c66f34ecbd9b160480df3a966c6fd67389b
Marouan El Marnissi <117521779+BeastzRage@users.noreply.github.com>
2026-07-27 19:50:05 +0200

    Added UserKNN to nearest-neighbour algorithms - Move UserKNN implementation into nearest_neighbour.py - Move UserKNN tests into test_nearest_neighbour.py - Updated the cosine similarity method to not transpose the matrix before computing similarities to make use of the method more intuitive - calls to the cosine similarity method have been updated to transpose the matrix prior to passing it to the method to keep results the same - Added supported similarity list to UserKNN following that of ItemKNN

--- recpack/algorithms/__init__.py
+++ recpack/algorithms/__init__.py
@@ -255,7 +255,7 @@ from recpack.algorithms.factorization_item_similarity import (
 from recpack.algorithms.ease import EASE
 from recpack.algorithms.slim import SLIM
 
-from recpack.algorithms.nearest_neighbour import ItemKNN, ItemPNN
+from recpack.algorithms.nearest_neighbour import ItemKNN, ItemPNN, UserKNN
 from recpack.algorithms.kunn import KUNN
 from recpack.algorithms.bprmf import BPRMF
 
@@ -287,5 +287,3 @@ from recpack.algorithms.time_aware_item_knn import (
 )
 
 from recpack.algorithms.sequential_rules import SequentialRules
-
-from recpack.algorithms.userknn import UserKNN
